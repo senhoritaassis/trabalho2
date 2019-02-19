4ee4aff0dbbefd70a274dd730f3c960a6110f221
1831038 <1831038@LABCMAQ01.itp.local>
2019-02-19 12:48:31 -0300

    ola, estou criando um repositorio

---
+++ aulaArray/ex1.php
@@ -0,0 +1,12 @@
+<?php
+
+    $vetor = array();
+    echo "teste";
+    for ($i = 0; $i <= 50; $i++) {
+        $vetor[$i]=42;
+       
+        echo 'Na posição: '.$i." tem o valor =".$vetor[$i]." <br>";
+        
+        
+    }
+?>
